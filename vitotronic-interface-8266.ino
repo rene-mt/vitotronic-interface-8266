0a7a1cad2c3ec7ec15c5e0e69a75525914dd6390
ReneMT <dev@renemt.de>
2016-04-01 10:04:51 +0200

    Optimized sketch to allow only one WiFi client

--- vitotronic-interface-8266.ino
+++ vitotronic-interface-8266.ino
@@ -1,24 +1,21 @@
 // Import required libraries
 #include "ESP8266WiFi.h"
 
-//how many clients should be able to telnet to this ESP8266
-#define MAX_SRV_CLIENTS 1
 //on which port the server should listen
 #define SRV_PORT 8888
-#define SRV_PORT_STR "8888"
 
 //WiFi credentials
 const char* ssid = "xxx";
 const char* password = "xxx";
-
-WiFiServer server(SRV_PORT);
-WiFiClient serverClients[MAX_SRV_CLIENTS];
 
 //Parameters for static IP configuration
 IPAddress ip(192, 168, 178, 210);    //IP address of the ESP8266
 IPAddress dns(192, 168,178, 1);      //DNS server
 IPAddress gateway(192, 168, 178, 1); //Gateway
 IPAddress subnet(255, 255, 255, 0);  //Subnet mask
+
+WiFiServer server(SRV_PORT);
+WiFiClient serverClient;
 
 void setup() {
   Serial1.begin(115200); // User Serial1 (GPIO2) as debug output (TX), with 115200,N,1
@@ -28,7 +25,8 @@ void setup() {
   
   // Initialize WiFi
   WiFi.mode(WIFI_STA);
-  WiFi.config(ip, dns, gateway, subnet); //set static IP
+  //if you are using DHCP please remove/comment the following line
+  WiFi.config(ip, dns, gateway, subnet); //Set static IP address. 
   WiFi.begin(ssid, password);
 
   Serial1.println("Vitotronic WiFi Interface"); Serial1.println();
@@ -42,54 +40,49 @@ void setup() {
   }
   Serial1.print("Ready! Server available at ");
   Serial1.print(WiFi.localIP());
-  Serial1.println(":" SRV_PORT_STR);
+  Serial1.print(": "); Serial1.print(SRV_PORT);Serial1.println();
 
   server.begin();
 }
 
 void loop() {
   uint8_t i;
-  //check if there are any new clients
+  //check if there is a new client trying to connect
   if (server.hasClient()) {
-    for (i = 0; i < MAX_SRV_CLIENTS; i++) {
-      //find free/disconnected spot
-      if (!serverClients[i] || !serverClients[i].connected()) {
-        if (serverClients[i]) serverClients[i].stop();
-        serverClients[i] = server.available();
-        Serial1.print("New client: "); Serial1.print(i); Serial1.println(); Serial1.println();
-        continue;
-      }
+    //check, if no client is already connected
+    if (!serverClient || !serverClient.connected()) {
+      if (serverClient) serverClient.stop();
+      serverClient = server.available();
+      Serial1.print("New client connected."); Serial1.println(); Serial1.println();
     }
-    //no free/disconnected spot so reject
-    WiFiClient serverClient = server.available();
-    serverClient.stop();
+    else {
+      //reject additional connection requests.
+      WiFiClient serverClient = server.available();
+      serverClient.stop();
+    }
   }
 
   yield();
 
-  //check clients for data
-  for (i = 0; i < MAX_SRV_CLIENTS; i++) {
-    if (serverClients[i] && serverClients[i].connected()) {
-      size_t len = serverClients[i].available();
-      if (len) {
-        uint8_t sbuf[len];
-        serverClients[i].read(sbuf, len);
+  //check client for data
+  if (serverClient && serverClient.connected()) {
+    size_t len = serverClient.available();
+    if (len) {
+      uint8_t sbuf[len];
+      serverClient.read(sbuf, len);
 
-        // Write received WiFi data to serial
-        Serial.write(sbuf, len);
-        yield();
+      // Write received WiFi data to serial
+      Serial.write(sbuf, len);
+      yield();
 
-        // Debug output received WiFi data to Serial1
-        Serial1.println();
-        Serial1.print("WiFi: ");
-        for (uint8_t n = 0; n < len; n++) {
-          Serial1.print(sbuf[n], HEX);
-        }
-        Serial1.println();
-        
-        yield();
+      // Debug output received WiFi data to Serial1
+      Serial1.println();
+      Serial1.print("WiFi: ");
+      for (uint8_t n = 0; n < len; n++) {
+        Serial1.print(sbuf[n], HEX);
       }
-    }
+      Serial1.println();
+     }
   }
 
   yield();
@@ -100,12 +93,10 @@ void loop() {
       uint8_t sbuf[len];
       Serial.readBytes(sbuf, len);
       
-      //push UART data to all connected WiFi clients
-      for(uint8_t i = 0; i < MAX_SRV_CLIENTS; i++){
-        if (serverClients[i] && serverClients[i].connected()){
-          serverClients[i].write(sbuf, len);
-          yield();
-        }
+      //push UART data to connected WiFi client
+      if (serverClient && serverClient.connected()){
+        serverClient.write(sbuf, len);
+        yield();
       }
 
       // Debug output received Serial data to Serial1
